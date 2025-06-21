56952bd662072c53e036cc0f7b3f70bcd96e69f3
th-neri <nerithiago123@gmail.com>
2025-06-20 20:10:16 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
